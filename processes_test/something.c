59cfb2622892730b8fd30b526238d0a6e373cc2e
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-03 12:44:27 +0000

    Separate 2 procs with wait function

--- processes_test/something.c
+++ processes_test/something.c
@@ -1,10 +1,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 int main(int argc, char* argv[]) {
-    printf("Hello World!");
+
+    pid_t pid = fork();
+    int n;
+    if (pid == 0) {
+        n = 1;
+    }
+    else {
+        n = 6;
+    }
+    if (pid != 0) {
+       wait(NULL); 
+    }
+    
+    for (int i=n; i<n+5; i++) {
+        printf("%d ", i);
+        fflush(stdout);
+    }
     return 0;
     
 }
