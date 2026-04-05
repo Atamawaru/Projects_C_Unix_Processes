5ff09c2523b8b8fbee87573b5a858480fa19d706
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-05 12:29:26 +0000

    Make 2 processes communicate with a pipe

---
+++ pipes_test/pipes_test.c
@@ -0,0 +1,32 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(int argc, char* argv[]) {
+    int fd[2];
+    if (pipe(fd)== -1){
+        printf("Error creating pipe\n;");
+        return 1;
+    }
+    int pid = fork();
+    if (pid == 0) {
+        close(fd[0]);
+        int x;
+        printf("Input a number: ");
+        scanf(" %d", &x);
+        write(fd[1], &x, sizeof(int));
+        close(fd[1]);
+    }
+    else {
+        close(fd[1]);
+        int y;
+        read(fd[0], &y, sizeof(int));
+        close(fd[0]);
+        printf("Got from child process %d\n", y);
+    }
+    return 0;
+    
+}
