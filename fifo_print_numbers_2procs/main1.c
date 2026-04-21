9e3b74f93a92a248ba768334c9ea525f93798a5f
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-21 17:59:40 +0000

    Open fifo for writing. Make executables run in parallel

--- fifo_print_numbers_2procs/main1.c
+++ fifo_print_numbers_2procs/main1.c
@@ -1,18 +1,30 @@
-#include <asm-generic/errno-base.h>
-#include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 int main(int argc, char* argv[]) {
-    
-    if (mkfifo("sum", 0777) == -1 && errno != EEXIST) {
+    char* fifo_name = "sum";
+    if (mkfifo(fifo_name, 0777) == -1 && errno != EEXIST) {
         perror("Error creating fifo: ");
+        return 1;
     }
-
+    int n = 5;
+    int nums[n];
+    srand(time(NULL));
+    for (int i=0; i<n; i++) {
+        nums[i]=rand() % 100 + 1;
+        printf("Generated num: %d\n", nums[i]);
+    }
+    int fdin = open(fifo_name, O_WRONLY);
+    if (fdin == -1) {
+        perror("Error opening fifo: ");
+        return 2;
+    }
+    close(fdin);
     return 0;
 }
